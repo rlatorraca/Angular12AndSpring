39e359ab1c526adaa78b6094c73885aaf46d5204
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-09-28 11:24:56 -0300

    Ammend last commit

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  // Atributes
+  public courses: Course[] = [
+    {_id: '1', name: 'Angular 12', category: 'Frontend'},
+    {_id: '2', name: 'Java 13', category: 'Java'},
+    {_id: '3', name: 'Java 17', category: 'Java'},
+    {_id: '4', name: 'SpringBoot', category: 'Java'},
+  ];
+  public displayedColumns: string[] = ['name', 'category'];
 
   constructor() { }
 
