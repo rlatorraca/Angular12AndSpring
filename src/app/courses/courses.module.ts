5cc78e06286622bbc2571eb9143e6e392012e348
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-10-06 18:14:25 -0300

    Add CSS for table | add card that wrap up the table.

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppImportsAngularMaterialModule } from '../shared/app-imports-angular-material/app-imports-angular-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
 
 
 @NgModule({
@@ -14,7 +13,7 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppImportsAngularMaterialModule
   ]
 })
 export class CoursesModule { }
